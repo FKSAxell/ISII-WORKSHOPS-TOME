54474d760c939596fb4c5b31e9d0086b75842deb
ltvargas <ltvargas@espol.edu.ec>
2020-10-22 17:49:56 -0500

    creating client class

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,5 +1,11 @@
 package ec.edu.espol.workshops.second;
 
+ 
 public class CarInsurance {
-
+  static final double BASEPREMIUM=500;
+	
+  public static void main(String[] args) {		
+    System.out.println("Calculate your BASE PREMIUM");
+  }
+  
 }
---
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/Client.java
@@ -0,0 +1,14 @@
+package ec.edu.espol.workshops.second;
+
+public class Client {
+	private int age;
+	private char sex;
+	private boolean maritalStatus; 
+	
+	public Client(int age,char sex,boolean maritalStatus) 
+	{
+		this.age=age;
+		this.sex=sex;
+		this.maritalStatus=maritalStatus;
+	}
+}
