8c3298cd29e05da7a00f90bbcd591183cae16a94
kelly <castrofigueroakelly@gmail.com>
2020-10-30 12:20:16 -0500

    PMD report

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,18 +1,39 @@
 package ec.edu.espol.workshops.second;
 
+/**
+ * The CarInsurance class is responsible for calculating the premium 
+ * that a user/customer/client must pay
+ *
+ *
+ */
 public class CarInsurance {
+	/**
+	 * BASEPREMIUM is the cost that applies to all clients in their premium
+	 */
 	public static double BASEPREMIUM=500;	
+	/**
+	 *It specifies whether customer's information is valid.
+	 */
 	public static boolean valido=true;
-	public static boolean aprobado=false;
+	/**
+	 *It specifies that the company approves the premium.
+	 */
+	public static boolean aprobado=true;
+	
+	/**
+	 * Verification of client data takes place to calculate the premium
+	 * @param args
+	 */
 	public static void main(String[] args) {			
 	    System.out.println("Calculate your BASE PREMIUM");
 	    Client client = new Client(28,'m', true,"A");
+	    CarInsurance insurance = new CarInsurance();
 	    if(client.getAge()>80 || !valido) {
 	    	System.out.println("No es posible vender");
 	    	System.out.println(-1);
 	    	System.exit(0);
 	    }
-	    calculatePremium(client);
+	    insurance.calculatePremium(client);
 	    if(!aprobado) {
 	    	System.out.println(-1);
 	    	System.exit(0);
@@ -22,7 +43,7 @@ public class CarInsurance {
 	}
 	
 	
-	private static void calculatePremium(Client cliente) {
+	private void calculatePremium(Client cliente) {
 		int age_client = cliente.getAge();
 		char gender_client = cliente.getSex();
 		boolean maritalStatus = cliente.isMaritalStatus();
@@ -46,7 +67,7 @@ public class CarInsurance {
 		}
 	
 	
-	 private static void checkCase3(int edad) {
+	 private void checkCase3(int edad) {
 			if ((edad>=45)&&(edad<65)) {
 				BASEPREMIUM -= 100;
 				aprobado=true;
@@ -54,7 +75,7 @@ public class CarInsurance {
 			
 	 }
 	 
-	 private static void calculateCase1(int age, boolean status) {
+	 private  void calculateCase1(int age, boolean status) {
 		 if(age<=25 && !(status)) {
 			 BASEPREMIUM+=1500;
 			 aprobado=true;
